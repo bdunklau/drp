325b35938ceab44dbbdb4f15adbd7251dad7e24e
Brent Dunklau <bdunklau@yahoo.com>
2020-02-08 16:02:20 -0600

    pass the model into the calculator

--- spec/drp_demo/DemoPricingSpec.ts
+++ spec/drp_demo/DemoPricingSpec.ts
@@ -15,7 +15,14 @@ describe("DemoPricing", function() {
   it("should determine the correct toll price", function() {
     let time = 1000; // 10am
     let locationId = 'abc';
-    let price = demoPricing.determineTollPrice(time, locationId);
+
+    let model/*: {locationId: string, time1: number, time2: number, price: number}[]*/ = [
+      {locationId: 'abc', time1: 900, time2: 1100, price: 1},
+      {locationId: 'abc', time1: 1101, time2: 1300, price: 2},
+      {locationId: 'def', time1: 900, time2: 1100, price: 3},
+    ]
+
+    let price = demoPricing.determineTollPrice(model, time, locationId);
     expect(price).toEqual(1);
   })
 
--- src/app/demo/demo-pricing/demo-pricing.calculator.ts
+++ src/app/demo/demo-pricing/demo-pricing.calculator.ts
@@ -10,12 +10,7 @@ DemoPricing.prototype.exists = function() {
   return true;
 };
 
-DemoPricing.prototype.determineTollPrice = function(time, locationId) {
-  var model/*: {locationId: string, time1: number, time2: number, price: number}[]*/ = [
-    {locationId: 'abc', time1: 900, time2: 1100, price: 1},
-    {locationId: 'abc', time1: 1101, time2: 1300, price: 2},
-    {locationId: 'def', time1: 900, time2: 1100, price: 3},
-  ]
+DemoPricing.prototype.determineTollPrice = function(model, time, locationId) {
 
   var loc = _.filter(model, function(item) {
     return item.locationId == locationId;
@@ -24,23 +19,5 @@ DemoPricing.prototype.determineTollPrice = function(time, locationId) {
   return found.price;
   // return 1;
 }
-
-//
-// Player.prototype.pause = function() {
-//   this.isPlaying = false;
-// };
-//
-// Player.prototype.resume = function() {
-//   if (this.isPlaying) {
-//     throw new Error("song is already playing");
-//   }
-//
-//   this.isPlaying = true;
-// };
-//
-// Player.prototype.makeFavorite = function() {
-//   this.currentlyPlayingSong.persistFavoriteStatus(true);
-// };
-//
 
 module.exports = DemoPricing;
