ceb2c3cc269009cccaf6c4874f057395390fbbea
Brent Dunklau <bdunklau@yahoo.com>
2020-02-12 08:44:53 -0600

    working on getting karma tests to pass using the Troubleshooting page from the YourVoteCounts wiki

--- src/app/demo/demo-charge/demo-charge.component.spec.ts
+++ src/app/demo/demo-charge/demo-charge.component.spec.ts
@@ -1,14 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DemoChargeComponent } from './demo-charge.component';
+import { DemoTravelComponent } from '../demo-travel/demo-travel.component';
+import { DemoTransactionsComponent } from '../demo-transactions/demo-transactions.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 
 describe('DemoChargeComponent', () => {
   let component: DemoChargeComponent;
   let fixture: ComponentFixture<DemoChargeComponent>;
 
+  const AngularFirestoreStub = {
+      collection: (name: string, f: (ref:any) => {}) => ({
+        doc: (_id: string) => ({
+          valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
+          set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+          ref: {
+            get: () => ({
+              data: () => ({
+                text: 'mock data',
+              }),
+            }),
+          }
+        }),
+        valueChanges: () => of([{id: '1', event: 'event1', date: {toDate: () => new Date()}}, // 2 mock LogEntry's
+                                {id: '2', event: 'event2', date: {toDate: () => new Date()}}]), // THIS IS NOT WHAT WE WANT TO MOCK HERE.
+        snapshotChanges: () => ({
+          pipe: (xxx) => {}
+        })
+      }),
+    };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DemoChargeComponent ]
+      declarations: [ DemoChargeComponent, DemoTravelComponent, DemoTransactionsComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [ {provide: AngularFirestore, useValue: AngularFirestoreStub} ]
     })
     .compileComponents();
   }));
--- src/app/demo/demo-pricing.service.spec.ts
+++ src/app/demo/demo-pricing.service.spec.ts
@@ -1,9 +1,45 @@
 import { TestBed } from '@angular/core/testing';
-
+// import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { DemoPricingService } from './demo-pricing.service';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 
 describe('DemoPricingService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  const AngularFirestoreStub = {
+      collection: (name: string, f: (ref:any) => {}) => ({
+        doc: (_id: string) => ({
+          valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
+          set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+          ref: {
+            get: () => ({
+              data: () => ({
+                text: 'mock data',
+              }),
+            }),
+          }
+        }),
+        valueChanges: () => of([{id: '1', event: 'event1', date: {toDate: () => new Date()}}, // 2 mock LogEntry's
+                                {id: '2', event: 'event2', date: {toDate: () => new Date()}}]), // THIS IS NOT WHAT WE WANT TO MOCK HERE.
+        snapshotChanges: () => ({
+          pipe: (xxx) => {}
+        })
+      }),
+    };
+
+
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ /*DemoChargeComponent, DemoTravelComponent, DemoTransactionsComponent*/ ],
+        //schemas: [NO_ERRORS_SCHEMA],
+        providers: [ {provide: AngularFirestore, useValue: AngularFirestoreStub} ]
+      })
+      .compileComponents();
+    }));
+
+
 
   it('should be created', () => {
     const service: DemoPricingService = TestBed.get(DemoPricingService);
